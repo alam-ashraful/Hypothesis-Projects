96caf3040d5c7a8e63a506c94c04f783a0885813
Md. Ashraful Alam <jameahmed125@yahoo.com>
2018-11-10 02:40:27 +0600

    Service Interfaces layer added

---
+++ Experiment.ServiceInterfaces/Base/IBaseService.cs
@@ -0,0 +1,18 @@
+﻿using Experiment.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment.ServiceInterfaces.Base
+{
+    public interface IBaseService<T> where T : BaseModel
+    {
+        IQueryable<T> All();
+        T Find(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+    }
+}
---
+++ Experiment.ServiceInterfaces/ICourseService.cs
@@ -0,0 +1,12 @@
+﻿using Experiment.Entities;
+using Experiment.ServiceInterfaces.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment.ServiceInterfaces
+{
+    public interface ICourseService : IBaseService<Course> { }
+}
