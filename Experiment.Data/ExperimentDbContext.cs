dabeffff2d32354a470b6b8acc1559e6d05b7208
Md. Ashraful Alam <jameahmed125@yahoo.com>
2018-11-10 01:45:40 +0600

    Data layer added again

---
+++ Experiment.Data/ExperimentDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Experiment.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Experiment.Data
+{
+    public class ExperimentDbContext : DbContext
+    {
+        public ExperimentDbContext() : base("ExDb") { }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Semester> Semesters { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
